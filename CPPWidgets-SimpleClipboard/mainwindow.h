41b9f7c6cd858d6d5d762e4d8c60acc5b92ee307
kronosGR <geo.elgeo@gmail.com>
2024-01-29 11:43:42 +0100

    fiinished simple clipboard manager

--- CPPWidgets-SimpleClipboard/mainwindow.h
+++ CPPWidgets-SimpleClipboard/mainwindow.h
@@ -1,7 +1,17 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QClipboard>
+#include <QCloseEvent>
+#include <QDebug>
+#include <QFile>
+#include <QFileDialog>
+#include <QList>
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QStringListModel>
+#include <QTextStream>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +27,34 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionOpen_triggered();
+    void on_actionSave_triggered();
+    void on_actionSave_As_triggered();
+    void on_actionStart_triggered();
+    void on_actionStop_triggered();
+    void on_actionCopy_triggered();
+    void on_actionCut_triggered();
+    void on_actionPaste_triggered();
+    void on_actionDelete_triggered();
+    void timeout();
+
+    void on_actionNew_triggered();
+
 private:
     Ui::MainWindow *ui;
+
+    QTimer m_timer;
+    QString m_last;
+    QStringListModel m_model;
+    QStringList m_list;
+    QString m_path;
+    bool m_changed;
+
+    void init();
+    void checkSaved();
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
 };
 #endif // MAINWINDOW_H
--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 ## 2. Simple ToDo - [source coude](https://github.com/kronosGR/QTpractice/tree/main/CPPWidgets-TODOlist)
 
 ![ToDo](screenshotToDo.png)
+
+## 3. Simple Clipboard Manager - [source code](https://github.com/kronosGR/QTpractice/tree/main/CPPWidgets-SimpleClipboard)
+
+![SCM](screenshotSimpleClipboard.png)
