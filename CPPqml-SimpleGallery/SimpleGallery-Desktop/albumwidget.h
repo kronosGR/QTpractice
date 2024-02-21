93291c38ea594d895770becffb17aac5ad8146f9
kronosGR <geo.elgeo@gmail.com>
2024-02-21 11:31:48 +0100

    album widget impl

--- CPPqml-SimpleGallery/SimpleGallery-Desktop/albumwidget.h
+++ CPPqml-SimpleGallery/SimpleGallery-Desktop/albumwidget.h
@@ -1,11 +1,17 @@
 #ifndef ALBUMWIDGET_H
 #define ALBUMWIDGET_H
 
+#include <QModelIndex>
 #include <QWidget>
 
 namespace Ui {
 class AlbumWidget;
 }
+
+class AlbumModel;
+class PictureModel;
+class QItemSelectionModel;
+class ThumbnailProxyModel;
 
 class AlbumWidget : public QWidget
 {
@@ -15,8 +21,29 @@ public:
     explicit AlbumWidget(QWidget *parent = nullptr);
     ~AlbumWidget();
 
+    void setAlbumModel(AlbumModel *albumModel);
+    void setAlbumSelectionModel(QItemSelectionModel *albumSelectionModel);
+    void setPictureModel(ThumbnailProxyModel *pictureModel);
+    void setPictureSelectionModel(QItemSelectionModel *selectionModel);
+
+signals:
+    void pictureActivated(const QModelIndex &index);
+
+private slots:
+    void deleteAlbum();
+    void editAlbum();
+    void addPictures();
+
 private:
     Ui::AlbumWidget *ui;
+
+    clearUi();
+    void loadAlbum(const QModelIndex &albumIndex);
+
+    AlbumModel *mAlbumModel;
+    QItemSelectionModel *mAlbumSelectionModel;
+    ThumbnailProxyModel *mPictureModel;
+    QItemSelectionModel *mPictureSelectionModel;
 };
 
 #endif // ALBUMWIDGET_H
--- CPPqml-SimpleGallery/SimpleGallery-Desktop/thumbnailproxymodel.cpp
+++ CPPqml-SimpleGallery/SimpleGallery-Desktop/thumbnailproxymodel.cpp
@@ -21,12 +21,12 @@ void ThumbnailProxyModel::setSourceModel(QAbstractItemModel *sourceModel)
     if (!sourceModel)
         return;
 
-    connect(sourceModel, &QAbstractItemModel::modelReset, [this] { reloadThumbnails(); })
-        connect(sourceModel,
-                &QAbstractItemModel::rowsInserted,
-                [this](const QModelIndex &parent, int first, int last) {
-                    generateThumbnails(index(first, 0), last - first + 1);
-                })
+    connect(sourceModel, &QAbstractItemModel::modelReset, [this] { reloadThumbnails(); });
+    connect(sourceModel,
+            &QAbstractItemModel::rowsInserted,
+            [this](const QModelIndex &parent, int first, int last) {
+                generateThumbnails(index(first, 0), last - first + 1);
+            });
 }
 
 PictureModel *ThumbnailProxyModel::pictureModel() const
