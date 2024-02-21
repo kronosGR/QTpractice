b14c0c0fea6906562a3d2dd2956d4d9b74951059
kronosGR <geo.elgeo@gmail.com>
2024-02-21 09:09:41 +0100

    added album widget

---
+++ CPPqml-SimpleGallery/SimpleGallery-Desktop/albumlistwidget.cpp
@@ -0,0 +1,51 @@
+#include "albumlistwidget.h"
+#include "ui_albumlistwidget.h"
+
+#include <QInputDialog>
+#include "albummodel.h"
+
+AlbumListWidget::AlbumListWidget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::AlbumListWidget)
+    , mAlbumModel(nullptr)
+{
+    ui->setupUi(this);
+
+    connect(ui->createAlbumButton, &QPushButton::clicked, this, &AlbumListWidget::createAlbum);
+}
+
+AlbumListWidget::~AlbumListWidget()
+{
+    delete ui;
+}
+
+void AlbumListWidget::setModel(AlbumModel *model)
+{
+    mAlbumModel = model;
+    ui->albumList->setModel(mAlbumModel);
+}
+
+void AlbumListWidget::setSelectionModel(QItemSelectionModel *selectionModel)
+{
+    ui->albumList->setSelectionModel(selectionModel);
+}
+
+void AlbumListWidget::createAlbum()
+{
+    if (!mAlbumModel)
+        return;
+
+    bool ok;
+    QString albumName = QInputDialog::getText(this,
+                                              "Create a new Album",
+                                              "Choose a name",
+                                              QLineEdit::Normal,
+                                              "New Album",
+                                              &ok);
+
+    if (ok && !albumName.isEmpty()) {
+        Album album(albumName);
+        QModelIndex createIndex = mAlbumModel->addAlbum(album);
+        ui->albumList->setCurrentIndex(createIndex);
+    }
+}
